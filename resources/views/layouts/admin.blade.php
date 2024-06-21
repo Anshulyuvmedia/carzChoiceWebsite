0b9f11c039190acf96d3cf593d866c1374174dcc
unknown <nitesh.yuvmedia@gmail.com>
2024-06-21 17:18:08 +0530

    Favicon Icon Changed

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{asset('assets/backend-assets/images/favicon.ico')}}">
+    <link rel="shortcut icon" href="{{asset('assets/backend-assets/images/car.png')}}">
 
     <!-- Bootstrap Css -->
     <link href="{{asset('assets/backend-assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet"
